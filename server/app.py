cfcd51487f22e79d03a4f146e541fe5dcee64021
Lauren <halpertln@gmail.com>
2023-07-07 11:55:34 -0600

    complete lab

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
@@ -9,7 +9,7 @@ from models import db, Plant
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = True
+app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
@@ -17,49 +17,32 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
+
     def get(self):
+        plants = [plant.to_dict() for plant in Plant.query.all()]
+        return make_response(jsonify(plants), 200)
 
-        response_dict_list = [plant.to_dict() for plant in Plant.query.all()]
-
-        response = make_response(
-            response_dict_list,
-            200,
-        )
-
-        return response
     def post(self):
+
         data = request.get_json()
-        new_record = Plant(
+
+        new_plant = Plant(
             name=data['name'],
             image=data['image'],
-            price=data['price']
+            price=data['price'],
         )
 
-        db.session.add(new_record)
+        db.session.add(new_plant)
         db.session.commit()
 
-        response_dict = new_record.to_dict()
-
-        response = make_response(
-            jsonify(response_dict),
-            201,
-        )
-
-        return response
+        return make_response(new_plant.to_dict(), 201)
 
 api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
     def get(self, id):
-
-        response_dict = Plant.query.filter_by(id=id).first().to_dict()
-
-        response = make_response(
-            jsonify(response_dict),
-            200,
-        )
-
-        return response
+        plant = Plant.query.filter_by(id=id).first().to_dict()
+        return make_response(jsonify(plant), 200)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
         
